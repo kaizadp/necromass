295192fedfd02e994046b619122997049fa7f772
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2024-01-09 19:40:02 -0800

    assign Whittaker Biome

--- 2-code/1-processing.R
+++ 2-code/1-processing.R
@@ -155,6 +155,69 @@ assign_climate_biome = function(dat){
   
   
 }
+assign_whittaker_biome = function(dat){
+  # code adapted from EBG, 2023
+  
+  shp = readShapePoly('1-data/geographic_databases/1b-terrestrial_ecosystems_teow/wwf_terr_ecos.shp')
+  
+  long.lat = 
+    dat %>% 
+    dplyr::select(rownumber, Longitude, Latitude) %>% 
+    rename(
+           longitude = Longitude,
+           latitude = Latitude) %>% 
+    mutate(latitude = as.numeric(latitude),
+           longitude = as.numeric(longitude)) %>% 
+    drop_na()
+  
+  
+  #https://stackoverflow.com/questions/44170102/find-in-which-polygon-the-points-in-a-map-are
+  coordinates(long.lat) <- ~ longitude + latitude
+  # # Set the projection of the SpatialPointsDataFrame using the projection of the shapefile
+  proj4string(long.lat) <- proj4string(shp)
+  poly = over(long.lat, shp)
+  
+  eco.meta = cbind(long.lat,poly,rep(NA, nrow(poly)));names(eco.meta)[ncol(eco.meta)] = 'biome_name'
+  eco.meta = as.data.frame(eco.meta)
+  #set biome names, from readme file
+  #1, Tropical & Subtropical Moist Broadleaf Forests
+  #2, Tropical & Subtropical Dry Broadleaf Forests
+  #3, Tropical & Subtropical Coniferous Forests
+  #4, Temperate Broadleaf & Mixed Forests
+  #5, Temperate Conifer Forests
+  #6, Boreal Forests/Taiga
+  #7, Tropical & Subtropical Grasslands, Savannas & Shrublands
+  #8, Temperate Grasslands, Savannas & Shrublands
+  #9, Flooded Grasslands & Savannas
+  #10, Montane Grasslands & Shrublands
+  #11, Tundra
+  #12, Mediterranean Forests, Woodlands & Scrub
+  #13, Deserts & Xeric Shrublands
+  #14, Mangroves
+  
+  biome = 
+    eco.meta %>% 
+    mutate(biome_name = case_when(
+      BIOME == 1 ~ "Tropical & Subtropical Moist Broadleaf Forests",
+      BIOME == 2 ~ "Tropical & Subtropical Dry Broadleaf Forests",
+      BIOME == 3 ~ "Tropical & Subtropical Coniferous Forests",
+      BIOME == 4 ~ "Temperate Broadleaf & Mixed Forests",
+      BIOME == 5 ~ "Temperate Conifer Forests",
+      BIOME == 6 ~ "Boreal Forests/Taigas",
+      BIOME == 7 ~ "Tropical & Subtropical Grasslands, Savannas & Shrublands",
+      BIOME == 8 ~ "Temperate Grasslands, Savannas & Shrublands",
+      BIOME == 9 ~ "Flooded Grasslands & Savannas",
+      BIOME == 10 ~ "Montane Grasslands & Shrublands",
+      BIOME == 11 ~ "Tundra",
+      BIOME == 12 ~ "Mediterranean Forests, Woodlands & Scrub",
+      BIOME == 13 ~ "Deserts & Xeric Shrublands",
+      BIOME == 14 ~ "Mangroves"
+    )) %>% 
+    dplyr::select(rownumber, biome_name)
+  
+  dat %>% left_join(biome)
+}
+
 
 clean_db = function(db_gsheets){
 
@@ -213,10 +276,11 @@ clean_db = function(db_gsheets){
     rename(Latitude = latitude, Longitude = longitude) %>% 
     clean_lat_lon() %>% 
     assign_climate_biome() %>% 
+    assign_whittaker_biome() %>% 
     mutate(ecosystem = tolower(ecosystem)) %>% 
     dplyr::select(rownumber, sample, notes,
                   Latitude, Longitude, lat_lon_notes, elevation_m, 
-                  MAT, MAP, ClimateTypes, everything())
+                  MAT, MAP, ClimateTypes, biome_name, everything())
   
   ## PROCESSING AND CALCULATING NECROMASS COLUMNS ----
   ## we have AS data and also some data as necromass.
