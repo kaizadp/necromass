9860fd692df74f6389d10a6938a555eab626087a
Kaizad Patel <50244730+kaizadp@users.noreply.github.com>
2024-07-16 08:53:31 -0700

    googlesheets::

--- run.R
+++ run.R
@@ -9,8 +9,7 @@
 ## Download the raw data from Google Drive
 ## This is commented out because it is not needed for each run
 ## Run these lines of code only if you need to download an updated version from Google Drive
-# db_gsheets = read_sheet("1nQc80bapNh3LI50Fdn-ybvKbSyyMs2jpc5SWMJ3Hy4c", 
-#                        sheet = "database", col_types = "c")
+# db_gsheets = googlesheets4::read_sheet("1nQc80bapNh3LI50Fdn-ybvKbSyyMs2jpc5SWMJ3Hy4c", sheet = "database", col_types = "c")
 # db_gsheets %>% write.csv("1-data/RAW-db_gsheets.csv", row.names = FALSE, na = "")
 
 targets::tar_make()
